2857a57733fdf177007768ab8a52b53b3ac1bcb9
kotocrm <kotocrm@cemim.com.br>
2014-06-13 12:18:55 -0300

    Update opportunities.php

--- opportunities.php
+++ opportunities.php
@@ -17,7 +17,7 @@
         <tr>
             <td style="text-align: center;"><input type="checkbox"></td>
             <td><a href="opportunity.php">Projeto de e-commerce</a></td>
-            <td><a href="account.php">DropComm Ltda</a></td>
+            <td><a href="account.php">Empresa 123 Ltda</a></td>
             <td>R$ 15.000</td>
             <td><?php echo array('Jessica', 'John', 'Liza')[rand(0,2)] ?></td>
         </tr>
